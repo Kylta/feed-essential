65a5b7eac9624acd9c858a127acb72a564895329
Kylta <christophe.bugnon02@gmail.com>
2019-08-26 23:12:34 +0200

    Remove extra white space

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -30,7 +30,6 @@ internal class FeedItemsMapper {
     
     private static var OK_200: Int { return 200 }
     
-    
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200,
             let root = try? JSONDecoder().decode(Root.self, from: data) else {
@@ -38,5 +37,5 @@ internal class FeedItemsMapper {
         }
         
         return .success(root.feed)
-    }    
+    }
 }
