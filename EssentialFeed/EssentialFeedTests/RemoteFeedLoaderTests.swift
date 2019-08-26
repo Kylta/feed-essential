55a2c9a09a374d0850927fa88e1568ee274adc05
Kylta <christophe.bugnon02@gmail.com>
2019-08-26 14:54:06 +0200

    Load request data from url with correct url

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,27 +8,50 @@
 
 import XCTest
 
-protocol HTTPClient {}
+protocol HTTPClient {
+    func get(from url: URL)
+}
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
     }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://any-url.com")!
         let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client)
+        let _ = RemoteFeedLoader(client: client, url: url)
         
         
         XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        let loader = RemoteFeedLoader(client: client, url: url)
+        
+        loader.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
