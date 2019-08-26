e8bd8a3c8d716c0b5d16c9cf184af432783911ac
Kylta <christophe.bugnon02@gmail.com>
2019-08-26 14:56:18 +0200

    Create helper to remove duplication and protect test from potential change in futur

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -29,22 +29,24 @@ class RemoteFeedLoader {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client, url: url)
-        
-        
+        let (_, client) = makeSUT()
+
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        let loader = RemoteFeedLoader(client: client, url: url)
+        let (sut, client) = makeSUT(url: url)
         
-        loader.load()
+        sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return (sut, client)
     }
     
     private class HTTPClientSpy: HTTPClient {
