c1d2519958487b7c59c43e40d0276137183cb0b4
Kylta <christophe.bugnon02@gmail.com>
2019-08-26 21:54:15 +0200

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -13,4 +13,18 @@ public struct FeedItem: Equatable {
     public let description: String?
     public let location: String?
     public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id, description, location
+        case imageURL = "image"
+    }
 }
